44e7261a99d04eadbe4e6fb629f8e9385305283c
Supriti Vijay <supriti.vijay@gmail.com>
2022-04-03 12:47:10 +0530

    linking based download - fix

--- app.py
+++ app.py
@@ -52,8 +52,9 @@ def app():
 			data.loc[len(data.index)] = [text_desc, name, location, time] 
 			
 			data.to_csv('complaint.csv', index = False)
+			csv = data.to_csv(index=False)
 
-			st.markdown(get_table_download_link(data), unsafe_allow_html=True)
+			st.markdown(get_table_download_link(csv), unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
